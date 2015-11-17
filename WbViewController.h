1efabd6f4248d07e20e9e8f3d833dae697cc9cf4
aaryaman <aaryaman.vir@gmail.com>
2015-11-17 16:13:41 -0500

    finished assignment 2

---
+++ WbViewController.h
@@ -0,0 +1,13 @@
+//
+//  WbViewController.h
+//  NavCtrl
+//
+//  Created by Aaryaman on 11/12/15.
+//  Copyright (c) 2015 Aditya Narayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WbViewController : UIViewController
+
+@end

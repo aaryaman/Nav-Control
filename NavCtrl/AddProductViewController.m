65c902d2556f24de4be022007ecca1a4e6cba9a6
aaryaman <aaryaman.vir@gmail.com>
2015-12-01 17:24:12 -0500

    Changed logic for adding companies and products so that they are separate processes

---
+++ NavCtrl/AddProductViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddProductViewController.m
+//  NavCtrl
+//
+//  Created by Aaryaman on 12/1/15.
+//  Copyright (c) 2015 Aditya Narayan. All rights reserved.
+//
+
+#import "AddProductViewController.h"
+
+@interface AddProductViewController ()
+
+@end
+
+@implementation AddProductViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
